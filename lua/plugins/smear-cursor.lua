c5f0e819b6c45ca20ceb76b3b49fbbd139de5f92
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2026-05-21 15:10:37 +0200

    animated cursor

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,15 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = true,
+    smear_horizontally = true,
+    smear_vertically = true,
+    smear_diagonally = true,
+    smear_to_cmd = true,
+    smear_insert_mode = true,
+
+    stiffness = 0.6,
+    trailing_stiffness = 0.5,
+  },
+}

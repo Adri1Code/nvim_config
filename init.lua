85dabfe0d53299aee4c36cb4af1a113fe0beccfe
Adrien Nicolas <103822035+AdrienBZH@users.noreply.github.com>
2026-08-22 08:28:46 +0200

    vim.loader.enable()

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+vim.loader.enable()
 local fontsize = 22
-
 vim.opt.guifont = "JetBrains Mono:h" .. fontsize
 vim.opt.tabstop = 8
 vim.opt.shiftwidth = 8
